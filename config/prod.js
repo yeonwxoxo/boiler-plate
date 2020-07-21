6b309f62c19c3a1fd8ecde3bc5ac0f8bd89ac8b2
Kangyeonwoo <sun_117@naver.com>
2020-07-21 22:45:59 +0900

    비밀정보보호

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
+
+//MONGO_URI 는 HEROKU에서 똑같이
--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
 
+//application/x-www-force-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yeonwoo:12345qwerty@cluster0.bid3e.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!하이'))
+
+app.post('/register', (req, res) => {
+    //회원가입할대 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+    
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
