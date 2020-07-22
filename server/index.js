e3d397a64b3c3528af6bec0dcaf05ac212eb2296
Kangyeonwoo <sun_117@naver.com>
2020-07-22 16:25:34 +0900

    32강 로그아웃까지완료

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -103,6 +102,6 @@ app.get('/api/users/logout', auth, (req, res) => {
 
 
 
+const port = 5000
 
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
